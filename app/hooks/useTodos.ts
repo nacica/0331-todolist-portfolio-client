af3b730db381f93f8c43308bce5f2f72c86684db
nacica <mikio.yokohama@gmail.com>
2024-03-31 07:51:07 +0900

    ポートフォリオ用アップロード

---
+++ app/hooks/useTodos.ts
@@ -0,0 +1,18 @@
+import useSWR from "swr";
+
+async function fetcher(key: string) {
+    return fetch(key).then((res) => res.json() );
+  }
+
+export const useTodos =()=>{
+        const { data, isLoading, error, mutate } = useSWR( 
+        "http://localhost:8000/allTodos", 
+        fetcher);
+
+        return {
+            todos:data,
+            isLoading,
+            error,
+            mutate
+        };
+};
